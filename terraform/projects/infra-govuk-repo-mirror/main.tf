ae72753864a096b78b6f5933d9f7fe7d207f7748
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2022-01-12 17:23:19 +0000

    [WIP] Update govuk-repo-mirror to use Jenkins job
    We are moving away from using Concourse

--- terraform/projects/infra-govuk-repo-mirror/main.tf
+++ terraform/projects/infra-govuk-repo-mirror/main.tf
@@ -21,19 +21,9 @@ variable "stackname" {
   description = "Stackname"
 }
 
-variable "jenkins_carrenza_staging_ssh_public_key" {
+variable "aws_environment" {
   type        = "string"
-  description = "The SSH public key of the Jenkins instance in the Carrenza staging environment"
-}
-
-variable "jenkins_carrenza_production_ssh_public_key" {
-  type        = "string"
-  description = "The SSH public key of the Jenkins instance in the Carrenza production environment"
-}
-
-variable "concourse_role_arn" {
-  type        = "string"
-  description = "The role ARN of the role that Concourse uses to assume the govuk_concourse_codecommit_role role"
+  description = "AWS Environment"
 }
 
 terraform {
@@ -52,64 +42,5 @@ resource "aws_iam_user" "govuk_codecommit_user" {
 
 resource "aws_iam_user_policy_attachment" "govuk_codecommit_user_policy_attachment" {
   user       = "${aws_iam_user.govuk_codecommit_user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
-}
-
-resource "aws_iam_user_ssh_key" "govuk_codecommit_user_jenkins_staging_ssh_key" {
-  username   = "${aws_iam_user.govuk_codecommit_user.name}"
-  encoding   = "SSH"
-  public_key = "${var.jenkins_carrenza_staging_ssh_public_key}"
-}
-
-resource "aws_iam_user_ssh_key" "govuk_codecommit_user_jenkins_production_ssh_key" {
-  username   = "${aws_iam_user.govuk_codecommit_user.name}"
-  encoding   = "SSH"
-  public_key = "${var.jenkins_carrenza_production_ssh_public_key}"
-}
-
-resource "aws_iam_user" "govuk_concourse_codecommit_user" {
-  name = "govuk-concourse-codecommit-user"
-}
-
-resource "aws_iam_user_policy_attachment" "govuk_concourse_codecommit_user_policy_attachment" {
-  user       = "${aws_iam_user.govuk_concourse_codecommit_user.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
-}
-
-resource "aws_iam_user_ssh_key" "govuk_concourse_codecommit_staging_user_ssh_key" {
-  username   = "${aws_iam_user.govuk_concourse_codecommit_user.name}"
-  encoding   = "SSH"
-  public_key = "${var.jenkins_carrenza_staging_ssh_public_key}"
-}
-
-resource "aws_iam_user_ssh_key" "govuk_concourse_codecommit_production_user_ssh_key" {
-  username   = "${aws_iam_user.govuk_concourse_codecommit_user.name}"
-  encoding   = "SSH"
-  public_key = "${var.jenkins_carrenza_production_ssh_public_key}"
-}
-
-resource "aws_iam_role" "govuk_concourse_codecommit_role" {
-  name = "govuk-concourse-codecommit-role"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "AWS": "${var.concourse_role_arn}"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "govuk_concourse_codecommit_role_policy_attachment" {
-  role       = "${aws_iam_role.govuk_concourse_codecommit_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
 }
